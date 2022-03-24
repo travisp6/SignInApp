885792a73875ced8332048d955def1f3d8e6fac1
travisp6 <96930736+travisp6@users.noreply.github.com>
2022-03-24 11:36:27 -0400

    Update loginginapp.py

--- loginginapp.py
+++ loginginapp.py
@@ -21,7 +21,6 @@ login_manager.login_view = "login_page"
 def load_user(user_id):
     return User.query.get(int(user_id))
 
-
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer(), primary_key=True)
     username = db.Column(db.String(length=40), unique=True, nullable=False)
